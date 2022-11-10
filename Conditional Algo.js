82ad24600e44cd7a160de04d2981e2d11973e22d
Jackie Noel <94089894+JackieNoel@users.noreply.github.com>
2022-11-10 06:42:49 -0600

    Add files via upload

---
+++ Conditional Algo.js
@@ -0,0 +1,11 @@
+
+var childHeight = 5
+function displayIfChildIsAbleToRideTheRollerCoaster(){
+    childHeight = true //is this necessary for the conditional to run?
+    if(childHeight>52){
+        console.log("Get on that ride, kid!")
+    }
+    else{
+        console.log("Sorry kiddo. Maybe next year.")
+    }
+}
